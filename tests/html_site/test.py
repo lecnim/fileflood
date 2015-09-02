0ac46a50832d351150ebe12448082ca43b7307e4
lecnim <kasper.minciel@gmail.com>
2015-09-03 00:41:37 +0200

    Fixed remove build before it was created.

--- tests/html_site/test.py
+++ tests/html_site/test.py
@@ -16,3 +16,19 @@ class Test(FunctionalTest):
         app.build()
 
         self.compare(app)
+
+
+    # def test_plugin(self):
+    #
+    #     def ext(app):
+    #         for page in app.find('*.html'):
+    #             if page.path != 'index.html':
+    #                 root, ext = os.path.splitext(page.path)
+    #                 page.path = root + '/' + 'index.html'
+    #
+    #
+    #
+    #
+    #     self.compare(
+    #         Flood(os.path.dirname(__file__)).use(ext).build()
+    #     )
