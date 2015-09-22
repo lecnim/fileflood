17a6528a7807e895ac65219d302b6f3a448f8008
lecnim <kasper.minciel@gmail.com>
2015-09-22 18:22:47 +0200

    Working on examples.

--- examples/simple_blog/script.py
+++ examples/simple_blog/script.py
@@ -1,15 +1,21 @@
-import os
+from os.path import dirname
 from rucola import Rucola
+
+# Used plugins:
 from rucola_markdown import Markdown
 from rucola_yamlfm import YamlFrontmatter
 from rucola_mustache import MustacheLayouts
 from rucola_permalinks import Permalinks
+from rucola_collections import Collections
 
-app = Rucola(os.path.dirname(__file__))
+# Set working directory relative to location of this file
+app = Rucola(dirname(__file__))
+# Remove previous build directory
 app.clear_output()
 
 app.use(
     YamlFrontmatter(),
+    Collections(),
     Markdown(),
     MustacheLayouts(),
     Permalinks()
